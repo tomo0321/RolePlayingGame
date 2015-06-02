9a9078222238be7e0a4823e31234e307c5f4baf5
Tomoyuki <Tomoyuki@192.168.100.102>
2015-06-02 21:11:06 +0900

    ああああああああああああああああああああああ

---
+++ RolePlayingGame/src/jp/f/tomoyuki/Charactor.java
@@ -0,0 +1,51 @@
+package jp.f.tomoyuki;
+
+public class Charactor {
+	private int hp; //HP
+	private String name; //名前
+	private int level; //レベル
+	private int power; //攻撃力
+	private int defence; //防御力
+	private int exp; //経験値
+
+
+
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public int getDefence() {
+		return defence;
+	}
+	public void setDefence(int defence) {
+		this.defence = defence;
+	}
+	public int getExp() {
+		return exp;
+	}
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
+
+
+}
