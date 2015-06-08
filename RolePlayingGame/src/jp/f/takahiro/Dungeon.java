71965760339cfc690554e4bc4a52f6776f8b7a2f
Tomooyuki <Tomooyuki@Tomooyuki-VAIO>
2015-06-09 00:08:39 +0900

    ダンジョン内の行動を記述

--- RolePlayingGame/src/jp/f/takahiro/Dungeon.java
+++ RolePlayingGame/src/jp/f/takahiro/Dungeon.java
@@ -1,22 +1,52 @@
 package jp.f.takahiro;
 
+import java.util.ArrayList;
+
+import jp.f.tomoyuki.Human;
 
 public class Dungeon extends Area {
-	private Floor currentFloor; // 現在のフロア
+	private ArrayList<Floor> floors; // フロアリスト
+	private int currentFloor; // 現在のフロア
 
-	public Dungeon() {
-		currentFloor = new Floor();
+	public Dungeon(Floor floor) {
+		floors = new ArrayList<Floor>();
+		floors.add(floor);
+		currentFloor = 1;
 	}
 
-	public Floor getCurrentFloor() {
+	public Dungeon(ArrayList<Floor> floors) {
+		this.floors = floors;
+		currentFloor = 1;
+	}
+
+	public int getCurrentFloor() {
 		return currentFloor;
 	}
 
+	public ArrayList<Floor> getFloors() {
+		return floors;
+	}
+
 	@Override
-	public void action() {
+	public void action(ArrayList<Human> party) {
 		// TODO 自動生成されたメソッド・スタブ
 		// ダンジョンでの行動
 
+		System.out.println("ここはダンジョンです．");
+
+		while (true) {
+			System.out.println("現在[" + currentFloor + "階]です．");
+
+			// このフロアの敵とバトル
+			//ButtleManager.buttle(floors.get(currentFloor), party);
+
+			// 最終フロアをクリアするか，諦めた場合は，ループを抜ける
+		}
 	}
 
+	@Override
+	public void chooseCommand() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
 }
